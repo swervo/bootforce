2f99bbe387aa4354746d617d480f6a5c9f7681b7
swervo <swervo@papersnail.co.uk>
2015-09-19 16:42:06 +0100

    initial pass through of tooltip.js

--- app/scripts/modules/tab.js
+++ app/scripts/modules/tab.js
@@ -6,7 +6,7 @@
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
 
- (function() {
+(function() {
     'use strict';
 
     require([
