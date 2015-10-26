2347f98bc81c1daab4fdbe59b935df0db2bb9d91
Dominic Butler <swervo@papersnail.co.uk>
2015-10-26 10:11:33 +0000

    fixing up do while statement

--- app/scripts/modules/tooltip.js
+++ app/scripts/modules/tooltip.js
@@ -515,8 +515,9 @@
         };
 
         Tooltip.prototype.getUID = function(prefix) {
-            do prefix += ~~(Math.random() * 1000000);
-            while (document.getElementById(prefix));
+            do {
+                prefix += ~~(Math.random() * 1000000);
+            } while (document.getElementById(prefix));
             return prefix;
         };
 
