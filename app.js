f916bf4e1a864b9235d6e664503af99c9213834e
Dominic Butler <swervo@papersnail.co.uk>
2015-10-26 18:23:42 +0000

    adding in node server for deployment

---
+++ app.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var app = require('commander');
+var debug = require('debug')('app');
+var Server = require('./server/server');
+
+var port = process.env.PORT || 4000;
+var serverURL = process.env.SERVER_URL || 'http://localhost:4000';
+
+app.version('0.1.0')
+    .usage('[options]')
+    .option('-p --port [port]', 'The port to listen on. [' + port + ']', port)
+    .option('--serverURL [url]', 'The full URL this server is reachable on. [' + serverURL + ']', serverURL)
+    .parse(process.argv);
+
+debug('');
+debug('============================================');
+debug('SLDS server will use the following settings');
+debug('============================================');
+debug();
+debug(' Port:                  ', parseInt(app.port, 10));
+debug(' Server URL:            ', app.serverURL);
+debug();
+debug('==========================================');
+debug('');
+
+var server = new Server(parseInt(app.port, 10),
+    app.serverURL,
+    app.silent
+);
+
+server.start(function (error) {
+    if (error) {
+        debug('Unable to start server. Reason: ', error);
+        process.exit(2);
+    }
+
+    debug('Server up and running on port ', app.port);
+});
---
+++ server/server.js
@@ -0,0 +1,74 @@
+/* jshint node:true */
+
+'use strict';
+
+var express = require('express');
+var http = require('http');
+var once = require('once');
+var path = require('path');
+var debug = require('debug')('main');
+
+module.exports = Server;
+
+function Server(port, serverURL, silent) {
+    this._port = port;
+    this._serverURL = serverURL;
+    this._silent = !!silent;
+    this.app = null;
+}
+
+Server.prototype._initialize = function (callback) {
+    this.app = express();
+    this.app.httpServer = http.createServer(this.app);
+
+
+    this.app.set('port', this._port);
+    this.app.use(express.static(path.join(__dirname, '../dist')));
+
+    callback(null);
+};
+
+Server.prototype._listen = function (callback) {
+
+    callback = once(callback);
+
+    this.app.httpServer.listen(this.app.get('port'), function (error) {
+        if (error) {
+            return callback(error);
+        }
+        callback();
+    });
+
+    this.app.httpServer.on('error', function (error) {
+        callback(error);
+    });
+};
+
+// public API
+Server.prototype.start = function (callback) {
+    var that = this;
+
+    if (this.app) {
+        return callback(new Error('Server is already up and running.'));
+    }
+
+    this._initialize(function (error) {
+        if (error) {
+            return callback(error);
+        }
+        that._listen(callback);
+    });
+};
+
+Server.prototype.stop = function (callback) {
+    var that = this;
+    if (!this.app.httpServer) {
+        return callback();
+    }
+
+    this.app.httpServer.close(function () {
+        that.app.httpServer.unref();
+        that.app = null;
+        callback();
+    });
+};
