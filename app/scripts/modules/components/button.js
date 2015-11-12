6a33b39e425fb0035d095d997b96a10e0751c5fa
Dominic Butler <swervo@papersnail.co.uk>
2015-11-12 13:35:41 +0000

    adding in support for stateful button

--- app/scripts/modules/components/button.js
+++ app/scripts/modules/components/button.js
@@ -23,36 +23,6 @@
 
         Button.VERSION = '3.3.5';
 
-        Button.DEFAULTS = {
-            loadingText: 'loading...'
-        };
-
-        Button.prototype.setState = function(state) {
-            var d = 'disabled';
-            var $el = this.$element;
-            var val = $el.is('input') ? 'val' : 'html';
-            var data = $el.data();
-
-            state += 'Text';
-
-            if (data.resetText === null) {
-                $el.data('resetText', $el[val]());
-            }
-
-            // push to event loop to allow forms to submit
-            setTimeout($.proxy(function() {
-                $el[val](data[state] === null ? this.options[state] : data[state]);
-
-                if (state === 'loadingText') {
-                    this.isLoading = true;
-                    $el.addClass(d).attr(d, d);
-                } else if (this.isLoading) {
-                    this.isLoading = false;
-                    $el.removeClass(d).removeAttr(d);
-                }
-            }, this), 0);
-        };
-
         Button.prototype.toggle = function() {
             var changed = true;
             var $parent = this.$element.closest('[data-toggle="buttons"]');
@@ -60,26 +30,32 @@
             if ($parent.length) {
                 if ($parent.data('grouptype') === 'checkbox') {
                     console.log('checkbox');
-                    if (this.$element.hasClass('active')) {
+                    if (this.$element.hasClass('slds-is-selected')) {
                         changed = false;
                     }
-                    this.$element.toggleClass('active');
+                    this.$element.toggleClass('slds-is-selected');
                 } else {
                     // assume its radio type behaviour
-                    if (this.$element.hasClass('active')) {
+                    if (this.$element.hasClass('slds-is-selected')) {
                         changed = false;
                     }
-                    $parent.find('.active').removeClass('active');
-                    this.$element.addClass('active');
+                    $parent.find('.slds-is-selected').removeClass('slds-is-selected');
+                    this.$element.addClass('slds-is-selected');
                 }
                 if (changed) {
                     //this may not be required
                     this.$element.trigger('change');
                 }
             } else {
-                this.$element.attr('aria-pressed', !this.$element.hasClass('active'));
-                this.$element.toggleClass('active');
+                // just a regular button
+                this.$element.attr('aria-pressed', !this.$element.hasClass('slds-is-selected'));
+                this.$element.toggleClass('slds-is-selected');
             }
+        };
+
+        Button.prototype.changeState = function() {
+            this.$element.attr('aria-pressed', !this.$element.hasClass('slds-is-selected'));
+            this.$element.toggleClass('slds-is-selected slds-not-selected');
         };
 
         // BUTTON PLUGIN DEFINITION
@@ -97,8 +73,9 @@
 
                 if (option === 'toggle') {
                     data.toggle();
-                } else if (option) {
-                    data.setState(option);
+                } else {
+                    // this is changestate
+                    data.changeState();
                 }
             });
         }
@@ -125,7 +102,11 @@
                 if (!$btn.hasClass('slds-button')) {
                     $btn = $btn.closest('.slds-button');
                 }
-                Plugin.call($btn, 'toggle');
+                if ($btn.data().buttontype) {
+                    Plugin.call($btn, 'changestate');
+                } else {
+                    Plugin.call($btn, 'toggle');
+                }
                 // doesnt look like this applies for slds
                 if (!($(e.target).is('input[type="radio"]') ||
                     $(e.target).is('input[type="checkbox"]'))) {
